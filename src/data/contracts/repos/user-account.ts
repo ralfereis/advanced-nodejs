ad59595e3ef78158ab80bb01dc918c5e104be31a
Ralfe Reis <ralfe.cavalcante@gmail.com>
2022-02-03 23:00:18 -0300

    feat: ensure Service calls TokenGenerator with correct params

--- src/data/contracts/repos/user-account.ts
+++ src/data/contracts/repos/user-account.ts
@@ -1,3 +1,4 @@
+
 export interface LoadUserAccountRepository {
   load: (params: LoadUserAccountRepository.Params) => Promise<LoadUserAccountRepository.Result>
 }
@@ -14,7 +15,7 @@ export namespace LoadUserAccountRepository {
 }
 
 export interface SaveFacebookAccountRepository {
-  saveWithFacebook: (params: SaveFacebookAccountRepository.Params) => Promise<void>
+  saveWithFacebook: (params: SaveFacebookAccountRepository.Params) => Promise<SaveFacebookAccountRepository.Result>
 }
 
 export namespace SaveFacebookAccountRepository {
@@ -23,5 +24,9 @@ export namespace SaveFacebookAccountRepository {
     email: string
     name: string
     facebookId: string
+  }
+
+  export type Result = {
+    id: string
   }
 }
