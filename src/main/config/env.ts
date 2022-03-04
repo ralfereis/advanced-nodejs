42f91e6ca3f6ea5901fca795c908d26df2b3b571
Ralfe Reis <ralfe.cavalcante@gmail.com>
2022-03-03 22:17:01 -0300

    feat: ensure FacebookApi returns undefined when HttpGetClient throws

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FB_CLIENT_ID ?? '647234076352251',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '636b4ef49ed304f43f45ea6f63153db8'
+  }
+}
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,35 @@
-describe('', () => {
-  it('', () => {
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
 
+describe('Facebook Api Integration Tests', () => {
+  it('should return a Facebook User if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'EAAJMpZB01HvsBAOLxwXd4ZAB9eOEAXDi1obcyrDCUSvFKgWtv9fGInQKzq78X71Brok9GRIAhXmObyLZBN5QV4jpmH68TI56stNaAB2N0QTcjDOvCZCkNsJLvxiXukSfjpGgL1Fh0fWUluzvMjAncWaWtuHj96v7CZBMy2UUoM2mmg4IKRsDZCyZATXyEe8mmqwvLIGMeZAv7ASVUqWSRAic' })
+
+    expect(fbUser).toEqual({
+      facebookId: '107248528574957',
+      email: 'node_opitjna_test@tfbnw.net',
+      name: 'Node Test'
+    })
+  })
+
+  it('should return undefined if token is invalid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'invalid' })
+
+    expect(fbUser).toBeUndefined()
   })
 })
