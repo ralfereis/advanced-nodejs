9dd0f6256bb7f4baab0091f19e96477f7a373276
Ralfe Reis <ralfe.cavalcante@gmail.com>
2022-03-29 22:48:48 -0300

    refactor: improve code quality

--- src/application/validation/composite.ts
+++ src/application/validation/composite.ts
@@ -7,9 +7,7 @@ export class ValidationComposite implements Validator {
   validate (): Error | undefined {
     for (const validator of this.validators) {
       const error = validator.validate()
-      if (error !== undefined) {
-        return error
-      }
+      if (error !== undefined) return error
     }
   }
 }
